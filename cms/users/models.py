5512e05adedf445c17ddd42c03891dac70ecf7c1
Manish Munikar <munikarmanish@gmail.com>
2017-01-23 15:49:31 +0545

    updated user model to include supervisor

--- cms/users/models.py
+++ cms/users/models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals, absolute_import
+from __future__ import absolute_import, unicode_literals
 
 from django.contrib.auth.models import AbstractUser
 from django.core.urlresolvers import reverse
@@ -13,7 +13,10 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = models.CharField(_('Name of User'), blank=True, max_length=255)
+    name = models.CharField(_('Name'), blank=True, max_length=255)
+    supervisor = models.ForeignKey(
+        'self', related_name='subordinate_list', null=True, default=None,
+        on_delete=models.SET_NULL, verbose_name=_('Supervisor'))
 
     def __str__(self):
         return self.username
